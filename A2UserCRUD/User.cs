62ebad60d0c9e76bda3eeb6c5fead588ca9e461e
Srison Kadariya <a37444@alunos.ipb.pt>
2020-03-23 22:25:59 +0000

    USER CRUD A2

---
+++ A2UserCRUD/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace A2UserCRUD
+{
+    public class User
+    {
+        public String ID { get; set; }
+        public String Username { get; set; }
+        public String Gender { get; set; }
+        public String Nationality { get; set; }
+        public String Password { get; set; }
+        public String Birthdate { get; set; }
+    }
+}
