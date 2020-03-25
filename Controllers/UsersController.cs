6355b729741f8bc30b1abbef5e0e69c81bb69602
Srison Kadariya <a37444@alunos.ipb.pt>
2020-03-25 01:18:09 +0000

    USERCRUD

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,55 @@
+﻿using A2UserCRUD.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace A2UserCRUD.Controller
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+
+        private ILogger _logger;
+        private IUsersService _service;
+
+
+        public UsersController(ILogger<UsersController> logger, IUsersService service)
+        {
+            _logger = logger;
+            _service = service;
+
+        }
+
+        [HttpGet("/api/users")]
+        public ActionResult<List<User>> GetUsers()
+        {
+            return _service.GetUsers();
+        }
+
+        [HttpPost("/api/users")]
+        public ActionResult<User> AddUser([FromBody]User user)
+        {
+            _service.AddUser(user);
+            return user;
+        }
+
+        [HttpPut("/api/users/{id}")]
+        public ActionResult<User> UpdateProduct(string id, [FromBody]User user)
+        {
+            _service.UpdateUser(id, user);
+            return user;
+        }
+
+        [HttpDelete("/api/users/{id}")]
+        public ActionResult<string> DeleteUser(string id)
+        {
+            _service.DeleteUser(id);
+            //_logger.LogInformation("products", _products);
+            return id;
+        }
+    }
+}
---
+++ Services/UsersService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace A2UserCRUD.Services
+{
+    public class UsersService : IUsersService
+    {
+        private List<User> _users;
+
+        public UsersService()
+        {
+            _users = new List<User>();
+        }
+
+        public User AddUser(User user)
+        {
+            _users.Add(user);
+            return user;
+            throw new NotImplementedException();
+        }
+
+        public string DeleteUser(string id)
+        {
+            for (var index = _users.Count - 1; index >= 0; index--)
+            {
+                if (_users[index].ID == id)
+                {
+                    _users.RemoveAt(index);
+                }
+            }
+
+            return id;
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetUsers()
+        {
+            return _users;
+            throw new NotImplementedException();
+        }
+
+        public User UpdateUser(string id, User user)
+        {
+            for (var index = _users.Count - 1; index >= 0; index--)
+            {
+                if (_users[index].ID == id)
+                {
+                    _users[index] = user;
+                }
+            }
+            return user;
+            throw new NotImplementedException();
+        }
+    }
+}
